fea32a7fd39ba2bb7bac22bfd8efb8214c44a766
sh2rbt9w4z-ai <sh2rbt9w4z@privaterelay.appleid.com>
2026-01-05 15:43:19 -0800

    Update echo.js

--- commands/echo.js
+++ commands/echo.js
@@ -1,13 +1,17 @@
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
+
 export default {
   data: new SlashCommandBuilder()
     .setName('echo')
     .setDescription('Echo a message')
-    .addStringOption(opt => opt.setName('message').setDescription('Message to echo').setRequired(true)),
+    .addStringOption(opt => opt
+      .setName('message')
+      .setDescription('Message to echo')
+      .setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
   async execute(interaction) {
-    const message = interaction.options.getString('message');
-
-    // Respond immediately
-    await interaction.reply({ content: message });
+    const msg = interaction.options.getString('message');
+    await interaction.reply({ content: msg });
   }
 };
