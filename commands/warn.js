be1cb778358d6f7f4f397d66509076567f4f7b35
sh2rbt9w4z-ai <sh2rbt9w4z@privaterelay.appleid.com>
2026-01-05 16:27:45 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -4,22 +4,37 @@ export default {
   data: new SlashCommandBuilder()
     .setName('warn')
     .setDescription('Warn a user with a reason')
-    .addUserOption(opt => opt.setName('target').setDescription('User to warn').setRequired(true))
-    .addStringOption(opt => opt.setName('reason').setDescription('Reason for warning').setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+    .addUserOption(opt =>
+      opt.setName('user')
+        .setDescription('User to warn')
+        .setRequired(true)
+    )
+    .addStringOption(opt =>
+      opt.setName('reason')
+        .setDescription('Reason for warning')
+        .setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
 
   async execute(interaction) {
-    const target = interaction.options.getUser('target');
+    const member = interaction.options.getMember('user');
     const reason = interaction.options.getString('reason');
 
-    // Delete your command message immediately
-    await interaction.deleteReply();
+    if (!member) {
+      return interaction.reply({ content: 'User not found.', ephemeral: true });
+    }
 
-    // Send warning to user
-    await interaction.followUp({ content: `You have been warned for: ${reason}`, ephemeral: true });
+    try {
+      // Send DM to warned user
+      await member.send(`You have been warned in **${interaction.guild.name}** for: **${reason}**`);
 
-    // Log warning in server (optional)
-    const logChannel = interaction.guild.channels.cache.get(interaction.guild.systemChannelId);
-    logChannel?.send(`**${target.tag}** was warned by **${interaction.user.tag}** for: ${reason}`);
+      // Reply to moderator privately
+      await interaction.reply({ content: `Successfully warned ${member.user.tag} for: ${reason}`, ephemeral: true });
+    } catch (err) {
+      console.error(err);
+      if (!interaction.replied) {
+        await interaction.reply({ content: 'Failed to warn user. They may have DMs off.', ephemeral: true });
+      }
+    }
   }
 };
