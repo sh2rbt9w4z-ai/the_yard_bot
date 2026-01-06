fbfc447a597b7cbe446506f184662df57bb95e98
sh2rbt9w4z-ai <sh2rbt9w4z@privaterelay.appleid.com>
2026-01-05 16:45:35 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -1,40 +1,32 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('warn')
-    .setDescription('Warn a user with a reason')
-    .addUserOption(opt =>
-      opt.setName('user')
-        .setDescription('User to warn')
-        .setRequired(true)
-    )
-    .addStringOption(opt =>
-      opt.setName('reason')
-        .setDescription('Reason for warning')
-        .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+    data: new SlashCommandBuilder()
+        .setName('warn')
+        .setDescription('Warn a member')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers)
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('User to warn')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('Reason for the warning')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const member = interaction.options.getMember('user');
+        const reason = interaction.options.getString('reason');
 
-  async execute(interaction) {
-    const member = interaction.options.getMember('user');
-    const reason = interaction.options.getString('reason');
+        if (!member) return interaction.reply({ content: 'Member not found.', ephemeral: true });
 
-    if (!member) {
-      return interaction.reply({ content: 'User not found.', ephemeral: true });
+        try {
+            await member.send(`You have been warned in ${interaction.guild.name} for: ${reason}`);
+            await interaction.reply({ content: `Warned ${member.user.tag} for: ${reason}`, ephemeral: true });
+        } catch (err) {
+            console.error(err);
+            await interaction.reply({ content: 'Failed to send warning.', ephemeral: true });
+        }
     }
-
-    try {
-      // Send DM to warned user
-      await member.send(`You have been warned in **${interaction.guild.name}** for: **${reason}**`);
-
-      // Reply to moderator privately
-      await interaction.reply({ content: `Successfully warned ${member.user.tag} for: ${reason}`, ephemeral: true });
-    } catch (err) {
-      console.error(err);
-      if (!interaction.replied) {
-        await interaction.reply({ content: 'Failed to warn user. They may have DMs off.', ephemeral: true });
-      }
-    }
-  }
 };
