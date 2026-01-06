0932397b7b1eb3251dc6e7600cdedeb74ab0fd0c
sh2rbt9w4z-ai <sh2rbt9w4z@privaterelay.appleid.com>
2026-01-05 16:26:43 -0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -3,15 +3,29 @@ import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 export default {
   data: new SlashCommandBuilder()
     .setName('purge')
-    .setDescription('Delete a number of messages')
-    .addIntegerOption(opt => opt.setName('amount').setDescription('Number of messages').setRequired(true))
+    .setDescription('Delete a number of messages from a channel')
+    .addIntegerOption(opt =>
+      opt.setName('amount')
+        .setDescription('Number of messages to delete (max 100)')
+        .setRequired(true)
+    )
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
   async execute(interaction) {
     const amount = interaction.options.getInteger('amount');
-    const channel = interaction.channel;
 
-    await channel.bulkDelete(amount, true);
-    return interaction.reply({ content: `Deleted ${amount} messages.`, ephemeral: true });
+    if (amount < 1 || amount > 100) {
+      return interaction.reply({ content: 'You can only delete between 1 and 100 messages.', ephemeral: true });
+    }
+
+    try {
+      // Delete messages (including command message)
+      const messages = await interaction.channel.messages.fetch({ limit: amount + 1 });
+      await interaction.channel.bulkDelete(messages, true);
+      await interaction.reply({ content: `Deleted ${messages.size - 1} messages.`, ephemeral: true });
+    } catch (err) {
+      console.error(err);
+      await interaction.reply({ content: 'Failed to purge messages. Make sure they are not older than 14 days.', ephemeral: true });
+    }
   }
 };
