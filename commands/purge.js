1ee6e57b92801ff099bd6a847f02ff2e3eefb644
sh2rbt9w4z-ai <sh2rbt9w4z@privaterelay.appleid.com>
2026-01-05 16:45:04 -0800

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,31 +1,26 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 
 export default {
-  data: new SlashCommandBuilder()
-    .setName('purge')
-    .setDescription('Delete a number of messages from a channel')
-    .addIntegerOption(opt =>
-      opt.setName('amount')
-        .setDescription('Number of messages to delete (max 100)')
-        .setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Delete a number of messages')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addIntegerOption(option =>
+            option.setName('amount')
+                .setDescription('Number of messages to delete (max 100)')
+                .setRequired(true)
+        ),
+    async execute(interaction) {
+        const amount = interaction.options.getInteger('amount');
+        if (amount < 1 || amount > 100) return interaction.reply({ content: 'Amount must be between 1 and 100.', ephemeral: true });
 
-  async execute(interaction) {
-    const amount = interaction.options.getInteger('amount');
-
-    if (amount < 1 || amount > 100) {
-      return interaction.reply({ content: 'You can only delete between 1 and 100 messages.', ephemeral: true });
+        try {
+            const messages = await interaction.channel.messages.fetch({ limit: amount });
+            await interaction.channel.bulkDelete(messages, true);
+            await interaction.reply({ content: `Deleted ${messages.size} messages.`, ephemeral: true });
+        } catch (err) {
+            console.error(err);
+            await interaction.reply({ content: 'Failed to delete messages.', ephemeral: true });
+        }
     }
-
-    try {
-      // Delete messages (including command message)
-      const messages = await interaction.channel.messages.fetch({ limit: amount + 1 });
-      await interaction.channel.bulkDelete(messages, true);
-      await interaction.reply({ content: `Deleted ${messages.size - 1} messages.`, ephemeral: true });
-    } catch (err) {
-      console.error(err);
-      await interaction.reply({ content: 'Failed to purge messages. Make sure they are not older than 14 days.', ephemeral: true });
-    }
-  }
 };
