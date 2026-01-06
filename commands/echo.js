9bf5a3aa1ca806c826aebb2aaff0bd3393da9c8d
sh2rbt9w4z-ai <sh2rbt9w4z@privaterelay.appleid.com>
2026-01-05 17:10:51 -0800

    Update echo.js

--- commands/echo.js
+++ commands/echo.js
@@ -1,16 +1,16 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName('echo')
-        .setDescription('Echo a message')
-        .addStringOption(option =>
-            option.setName('message')
-                .setDescription('Message to echo')
-                .setRequired(true)
-        ),
-    async execute(interaction) {
-        const msg = interaction.options.getString('message');
-        await interaction.reply({ content: msg });
-    }
+  data: new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Echo a message')
+    .addStringOption(option => option
+      .setName('message')
+      .setDescription('Message to echo')
+      .setRequired(true)),
+
+  async execute(interaction) {
+    const message = interaction.options.getString('message');
+    await interaction.reply({ content: message, ephemeral: true });
+  }
 };
